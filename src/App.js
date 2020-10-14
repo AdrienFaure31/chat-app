304ae74c52b6124650d23b7aa15aaa8aa2a79176
Faure <adri3196.af@gmail.com>
2020-10-14 09:19:35 +0200

    premier test

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Contact from './components/Contact.js';
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <Contact name='Christina Payne' image='https://randomuser.me/api/portraits/women/0.jpg' online/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Contact.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style.css'
+
+
+
+const Contact = (props) => (
+    <div className="Contact">
+        <img src={props.image} alt="Contact" className="avatar"/>
+        <div>
+            <h3 className="name">{props.name}</h3>
+            <div className="status">
+                <div className={props.online ? 'status-online' : 'status-offline'}></div>
+                <p className='status-text'>{props.online ? 'Online' : 'Offline'}</p>
+            </div>
+        </div>
+    </div>
+);
+
+
+Contact.propTypes = {
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    online: PropTypes.bool
+}
+
+
+export default Contact;
