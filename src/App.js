522c5aedd95feb494009166860028cf0640afbb5
Faure <adri3196.af@gmail.com>
2020-10-26 09:29:12 +0100

    contactlist

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Contact from './components/Contact.js';
+import ContactList from './components/ContactList';
 
 
 
@@ -8,6 +9,8 @@ function App() {
   return (
     <div className="App">
      <Contact name='Christina Payne' image='https://randomuser.me/api/portraits/women/0.jpg' online/>
+     <Contact name='Sophia Jackson' image='https://randomuser.me/api/portraits/women/34.jpg' online/>
+     <Contact name='Diane Garcia' image='https://randomuser.me/api/portraits/women/57.jpg' />
     </div>
   );
 }
