e1248af6456507cfed4b17ad1e986d7ea372ec00
Faure <adri3196.af@gmail.com>
2020-10-26 11:03:31 +0100

    statecontact

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Contact from './components/Contact.js';
+import './App.css';
 import ContactList from './components/ContactList';
 
 
@@ -8,9 +8,7 @@ import ContactList from './components/ContactList';
 function App() {
   return (
     <div className="App">
-     <Contact name='Christina Payne' image='https://randomuser.me/api/portraits/women/0.jpg' online/>
-     <Contact name='Sophia Jackson' image='https://randomuser.me/api/portraits/women/34.jpg' online/>
-     <Contact name='Diane Garcia' image='https://randomuser.me/api/portraits/women/57.jpg' />
+     <ContactList/>
     </div>
   );
 }
